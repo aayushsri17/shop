e23b435cf95b9eb167688d31ff1000d8675c28e7
aayushsri17 <aayushsri17@gmail.com>
2017-07-03 04:07:51 +0530

    Revert "shop"
    This reverts commit 4d970074ba74aa729c25fb56479b402b7e4ce938.

--- 3/error.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<head>
-		<title>Error In Login</title>
-	</head>
-	<body>
-	Please enter E-mail id and Password correctly. Go Back to the website <a href="index.php">here</a>.
-	</body>
-</html>
